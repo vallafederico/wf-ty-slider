40d6c9960f8179714d27abc033187541964a0b27
vallafederico <valla.federico@gmail.com>
2022-12-19 13:09:08 +0100

    added css

---
+++ src/warns.js
@@ -0,0 +1,11 @@
+export default function warnError(type) {
+  const fill = {
+    missing: "WRAPPER",
+  };
+
+  switch (type) {
+    case "noWrapper":
+  }
+
+  const message = `Slider is Missing something!`;
+}
